bf7e124c815811c3cb34bb03c3cc598ed7da41b6
dvarfe <skt3032@yandex.ru>
2026-02-26 23:37:02 +0300

    fix(agg): update aggregation

--- src/train_scripts/Agg.py
+++ src/train_scripts/Agg.py
@@ -12,11 +12,11 @@ from typing import List, Dict
 import pandas as pd
 import numpy as np
 
-from disk_analyzer.stages.model_scoring import ModelScorer
+from scoring import ModelScorer
 from torch.utils.data import DataLoader
-from disk_analyzer.models.Dataset import DiskDataset
-from Experiments import TIMES, TRAIN_BATCHSIZE
-from PredictionsAggregator import PredictionsAggregator
+from utils.Dataset import DiskDataset
+from .Experiments import TIMES, TRAIN_BATCHSIZE
+from aggregation.PredictionsAggregator import PredictionsAggregator
 
 np.random.seed(42)
 
@@ -31,68 +31,6 @@ def create_schema(base_schema, metrics_list, model_name):
     # elif model_name == 'SP':
     #     ret_schema['hidden_dim'] = []
     return ret_schema
-
-
-BASE_SCHEMA: Dict[str, list] = {
-    'train_samples': [],
-    'agg_samples': [],
-    'method': [],
-    'agg_method': [],
-    'agg_weight': [],
-    'model_id': []
-}
-
-METRICS_LIST = {'ci', 'ibs'}
-DATASET_TRAIN_SAMPLES = 20
-MODEL_TRAIN_SAMPLES = 20
-# SAMPLE_GRID = np.arange(1, 10)
-SAMPLE_GRID = [1, 10]
-EXP_NUM = 106
-BASE_EXP_NUM = 105  # Exp number from which fitted models are taken
-DATA_FOLDER = os.path.join("Data", "Preprocessed_new")
-RES_FOLDER = os.path.join("Artifacts", f"Exp_{EXP_NUM}")
-MODELS_FOLDER = os.path.join(RES_FOLDER, "models")
-RES_FILENAME = os.path.join(
-    RES_FOLDER, f"Agg_{DATASET_TRAIN_SAMPLES}_{MODEL_TRAIN_SAMPLES}_{max(SAMPLE_GRID)}.csv")
-TEST_SAMPLES = [25]
-MODELS_SIZE = [2048]
-MODEL_NAME = 'DDH'
-SCHEMA = create_schema(BASE_SCHEMA, METRICS_LIST, MODEL_NAME)
-DATA_EXT = 'csv'
-EXP_DESC = f"Агрегация {MODEL_NAME}"
-
-AGGREGATORS_DICT = {
-    "n_dist": {
-        "0.01": PredictionsAggregator(mode='n_dist', weight=0.01),
-        "0.1": PredictionsAggregator(mode='n_dist', weight=0.1),
-        "0.3": PredictionsAggregator(mode='n_dist', weight=0.3),
-        "0.5": PredictionsAggregator(mode='n_dist', weight=0.5),
-        "0.7": PredictionsAggregator(mode='n_dist', weight=0.7),
-        "0.9": PredictionsAggregator(mode='n_dist', weight=0.9),
-        "0.99": PredictionsAggregator(mode='n_dist', weight=0.99)
-    },
-    "t_dist": {
-        "0.1": PredictionsAggregator(mode='t_dist', weight=0.1),
-        "1": PredictionsAggregator(mode='t_dist', weight=1),
-        "10": PredictionsAggregator(mode='t_dist', weight=10),
-        "25": PredictionsAggregator(mode='t_dist', weight=25),
-        "50": PredictionsAggregator(mode='t_dist', weight=50),
-        "100": PredictionsAggregator(mode='t_dist', weight=100),
-        "1000": PredictionsAggregator(mode='t_dist', weight=1000)
-    },
-    "prob_dist": {
-        "-1": PredictionsAggregator(mode='prob_dist'),
-    },
-    "geom": {
-        "0.01": PredictionsAggregator(mode='geom', weight=0.01),
-        "0.1": PredictionsAggregator(mode='geom', weight=0.1),
-        "0.3": PredictionsAggregator(mode='geom', weight=0.3),
-        "0.5": PredictionsAggregator(mode='geom', weight=0.5),
-        "0.7": PredictionsAggregator(mode='geom', weight=0.7),
-        "0.9": PredictionsAggregator(mode='geom', weight=0.9),
-        "0.99": PredictionsAggregator(mode='geom', weight=0.99)
-    },
-}
 
 
 def create_res_file(filename):
@@ -181,17 +119,17 @@ def get_extended_times(df, agg_dict, times):
     return times_extended
 
 
-def calculate_metrics(model, predictions_aggregator, scorer, method, weight, n_samples, timeshift, X, X_gt, metric_postfix='test', metrics_list=['ci', 'ibs'], df_train_gt=None):
-    aggregated_pred = predictions_aggregator.predict(X, TIMES, timeshift=timeshift)
+def calculate_metrics(model, predictions_aggregator, scorer, method, weight, n_samples, timeshift, X, X_gt, model_name, model_train_samples, times, metric_postfix='test', metrics_list=['ci', 'ibs'], df_train_gt=None):
+    aggregated_pred = predictions_aggregator.predict(X, times, timeshift=timeshift)
 
     metrics = scorer.get_metrics(
-        model, aggregated_pred, X_gt, TIMES,
+        model, aggregated_pred, X_gt, times,
         metrics=metrics_list,
         df_train=df_train_gt)
 
     # Формируем запись результата
     result_row = {
-        'train_samples': MODEL_TRAIN_SAMPLES,
+        'train_samples': model_train_samples,
         'agg_samples': n_samples,
         'method': model_name.split('_')[1],
         'agg_method': method,
@@ -206,7 +144,7 @@ def calculate_metrics(model, predictions_aggregator, scorer, method, weight, n_s
     return result_row
 
 
-def eval_model(data_path, model_name, agg_dict, metrics_list, metric_postfix='test'):
+def eval_model(data_path, data_folder, models_folder, model_name, agg_dict, metrics_list, sample_grid, times, model_train_samples, metric_postfix='test', data_ext='.csv', train_batchsize=512):
     """
     Оценивает модель на данных с различными методами агрегации.
 
@@ -222,7 +160,7 @@ def eval_model(data_path, model_name, agg_dict, metrics_list, metric_postfix='te
     """
 
     # Загружаем данные
-    if DATA_EXT == 'csv':
+    if data_ext == '.csv':
         df_data = pd.read_csv(data_path)
     else:
         df_data = pd.read_parquet(data_path)
@@ -233,14 +171,14 @@ def eval_model(data_path, model_name, agg_dict, metrics_list, metric_postfix='te
 
     dl_data = DataLoader(
         dataset=DiskDataset('score', [data_path]),
-        batch_size=TRAIN_BATCHSIZE)
+        batch_size=train_batchsize)
 
-    times_extended = get_extended_times(df_data, agg_dict, TIMES)
+    times_extended = get_extended_times(df_data, agg_dict, times)
 
     # Загружаем train данные если нужна метрика iauc
     if 'iauc' in metrics_list:
-        train_path = os.path.join(DATA_FOLDER, f'1_train_preprocessed.{DATA_EXT}')
-        if DATA_EXT == 'csv':
+        train_path = os.path.join(data_folder, f'1_train_preprocessed{data_ext}')
+        if data_ext == '.csv':
             df_train = pd.read_csv(train_path)
         else:
             df_train = pd.read_parquet(train_path)
@@ -249,7 +187,7 @@ def eval_model(data_path, model_name, agg_dict, metrics_list, metric_postfix='te
         df_train_gt = None
 
     # Загружаем модель
-    model_path = os.path.join(MODELS_FOLDER, f'{model_name}')
+    model_path = os.path.join(models_folder, f'{model_name}')
     if os.path.exists(f'{model_path}_model.pkl'):
         model = load_model(f'{model_path}_model.pkl')
     else:
@@ -264,16 +202,16 @@ def eval_model(data_path, model_name, agg_dict, metrics_list, metric_postfix='te
             print(f'Начало обработки {n_samples} n_samples')
             time_start = time.time()
 
-            X_pred, X_gt = model.predict(dl_data, times=TIMES, agg_horizon=n_samples, trunc_right=max(SAMPLE_GRID))
+            X_pred, X_gt = model.predict(dl_data, times=times, agg_horizon=n_samples, trunc_right=max(sample_grid))
 
             metrics = scorer.get_metrics(
-                model, X_pred, X_gt, TIMES,
+                model, X_pred, X_gt, times,
                 metrics=metrics_list,
                 df_train=df_train_gt)
 
             # Формируем запись результата
             result_row = {
-                'train_samples': MODEL_TRAIN_SAMPLES,
+                'train_samples': model_train_samples,
                 'agg_samples': n_samples,
                 'method': model_name.split('_')[1],
                 'agg_method': 'DDH',
@@ -291,13 +229,13 @@ def eval_model(data_path, model_name, agg_dict, metrics_list, metric_postfix='te
         X_pred = X_pred.sort_values(by=['serial_number', 'time'])
         X_gt = X_gt.sort_values(by=['serial_number', 'time'])
 
-        sampled_predictions = sample_first_observations(X_pred, SAMPLE_GRID)
+        sampled_predictions = sample_first_observations(X_pred, sample_grid)
         scorer = ModelScorer()
 
         # Список для накопления результатов
         results_list = []
 
-        for n_samples in SAMPLE_GRID:
+        for n_samples in sample_grid:
             print(f'Начало обработки {n_samples} n_samples')
             time_start = time.time()
 
@@ -312,7 +250,7 @@ def eval_model(data_path, model_name, agg_dict, metrics_list, metric_postfix='te
                 for weight in agg_dict[method]:
 
                     result_row = calculate_metrics(model, agg_dict[method][weight], scorer, method, weight, n_samples,
-                                                   cur_timeshift, cur_X_pred, cur_X_gt,
+                                                   cur_timeshift, cur_X_pred, cur_X_gt, model_name, model_train_samples, times,
                                                    metric_postfix, metrics_list, df_train_gt)
                     results_list.append(result_row)
 
@@ -329,32 +267,103 @@ if __name__ == "__main__":
     с помощью sample_first_observation.
     Эти кусочки уже агрегируем, передавая их в pred_agg.
     """
+    BASE_SCHEMA: Dict[str, list] = {
+        'train_samples': [],
+        'agg_samples': [],
+        'method': [],
+        'agg_method': [],
+        'agg_weight': [],
+        'model_id': []
+    }
 
+    METRICS_LIST = {'ci', 'ibs'}
+    DATASET_TRAIN_SAMPLES = 20
+    MODEL_TRAIN_SAMPLES = 20
+    # SAMPLE_GRID = np.arange(1, 10)
+    SAMPLE_GRID = [1, 10]
+    EXP_NUM = 106
+    BASE_EXP_NUM = 105  # Exp number from which fitted models are taken
+    DATA_FOLDER = os.path.join("Data", "Preprocessed_new")
+    RES_FOLDER = os.path.join("Artifacts", f"Exp_{EXP_NUM}")
+    MODELS_FOLDER = os.path.join(RES_FOLDER, "models")
+    RES_FILENAME = os.path.join(
+        RES_FOLDER, f"Agg_{DATASET_TRAIN_SAMPLES}_{MODEL_TRAIN_SAMPLES}_{max(SAMPLE_GRID)}.csv")
+    TEST_SAMPLES = [25]
+    MODELS_SIZE = [2048]
+    MODEL_NAME = 'DDH'
+    SCHEMA = create_schema(BASE_SCHEMA, METRICS_LIST, MODEL_NAME)
+    DATA_EXT = '.csv'
+    EXP_DESC = f"Агрегация {MODEL_NAME}"
+
+    AGGREGATORS_DICT = {
+        "n_dist": {
+            "0.01": PredictionsAggregator(mode='n_dist', weight=0.01),
+            "0.1": PredictionsAggregator(mode='n_dist', weight=0.1),
+            "0.3": PredictionsAggregator(mode='n_dist', weight=0.3),
+            "0.5": PredictionsAggregator(mode='n_dist', weight=0.5),
+            "0.7": PredictionsAggregator(mode='n_dist', weight=0.7),
+            "0.9": PredictionsAggregator(mode='n_dist', weight=0.9),
+            "0.99": PredictionsAggregator(mode='n_dist', weight=0.99)
+        },
+        "t_dist": {
+            "0.1": PredictionsAggregator(mode='t_dist', weight=0.1),
+            "1": PredictionsAggregator(mode='t_dist', weight=1),
+            "10": PredictionsAggregator(mode='t_dist', weight=10),
+            "25": PredictionsAggregator(mode='t_dist', weight=25),
+            "50": PredictionsAggregator(mode='t_dist', weight=50),
+            "100": PredictionsAggregator(mode='t_dist', weight=100),
+            "1000": PredictionsAggregator(mode='t_dist', weight=1000)
+        },
+        "prob_dist": {
+            "-1": PredictionsAggregator(mode='prob_dist'),
+        },
+        "geom": {
+            "0.01": PredictionsAggregator(mode='geom', weight=0.01),
+            "0.1": PredictionsAggregator(mode='geom', weight=0.1),
+            "0.3": PredictionsAggregator(mode='geom', weight=0.3),
+            "0.5": PredictionsAggregator(mode='geom', weight=0.5),
+            "0.7": PredictionsAggregator(mode='geom', weight=0.7),
+            "0.9": PredictionsAggregator(mode='geom', weight=0.9),
+            "0.99": PredictionsAggregator(mode='geom', weight=0.99)
+        },
+    }
     init_experiments_folder(BASE_EXP_NUM)
     model_name = '0_DDH'
 
-    train_path = os.path.join(DATA_FOLDER, f'{DATASET_TRAIN_SAMPLES}_train_preprocessed.{DATA_EXT}')
+    train_path = os.path.join(DATA_FOLDER, f'{DATASET_TRAIN_SAMPLES}_train_preprocessed{DATA_EXT}')
 
     train_res = eval_model(
         data_path=train_path,
+        data_folder=DATA_FOLDER,
+        models_folder=MODELS_FOLDER,
         model_name=model_name,
         agg_dict=AGGREGATORS_DICT,
         metrics_list=METRICS_LIST,
-        metric_postfix='train'
+        sample_grid=SAMPLE_GRID,
+        times=TIMES,
+        model_train_samples=MODEL_TRAIN_SAMPLES,
+        metric_postfix='train',
+        data_ext=DATA_EXT
     )
 
-    test_path = os.path.join(DATA_FOLDER, f'{DATASET_TRAIN_SAMPLES}_{TEST_SAMPLES[0]}_test_preprocessed.{DATA_EXT}')
+    test_path = os.path.join(DATA_FOLDER, f'{DATASET_TRAIN_SAMPLES}_{TEST_SAMPLES[0]}_test_preprocessed{DATA_EXT}')
 
     test_res = eval_model(
-        data_path=test_path,
+        data_path=train_path,
+        data_folder=DATA_FOLDER,
+        models_folder=MODELS_FOLDER,
         model_name=model_name,
         agg_dict=AGGREGATORS_DICT,
         metrics_list=METRICS_LIST,
-        metric_postfix='test'
+        sample_grid=SAMPLE_GRID,
+        times=TIMES,
+        model_train_samples=MODEL_TRAIN_SAMPLES,
+        metric_postfix='test',
+        data_ext=DATA_EXT
     )
 
     results_df = pd.merge(train_res, test_res, how='outer', on=[
-                          'agg_samples', 'train_samples', 'method', 'agg_method', 'agg_weight', 'model_id'])
+        'agg_samples', 'train_samples', 'method', 'agg_method', 'agg_weight', 'model_id'])
 
     # Сохраняем результаты
     results_df.to_csv(RES_FILENAME, index=False)
